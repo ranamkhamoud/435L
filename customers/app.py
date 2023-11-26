c7cbee64139e4bcbf81794247edb267f58f39fe0
ranamhamoud <94687111+ranamhamoud@users.noreply.github.com>
2023-11-26 14:33:19 +0200

    added customer api

---
+++ customers/app.py
@@ -0,0 +1,137 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
+from werkzeug.security import generate_password_hash
+import os
+
+app = Flask(__name__)
+
+# Database Configuration
+base_dir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(base_dir, '..', 'db', 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+# Customer Model
+class Customer(db.Model):
+    username = db.Column(db.String(80), primary_key=True, unique=True)
+    full_name = db.Column(db.String(150), nullable=False)
+    password_hash = db.Column(db.String(128))
+    age = db.Column(db.Integer)
+    address = db.Column(db.String(200))
+    gender = db.Column(db.String(50))
+    marital_status = db.Column(db.String(50))
+    wallet = db.Column(db.Float, default=0.0)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def __repr__(self):
+        return f'<Customer {self.username}>'
+
+# Create the database tables
+with app.app_context():
+    db.create_all()
+
+# Routes
+@app.route('/')
+def home():
+    return "Welcome to the Customer Service API!"
+
+@app.route('/register', methods=['POST'])
+def register_customer():
+    data = request.json
+    required_fields = ['username', 'full_name', 'password', 'age', 'address', 'gender', 'marital_status']
+    if not all(field in data for field in required_fields):
+        return jsonify({'error': 'All fields are required'}), 400
+
+    if db.session.get(Customer, data['username']):
+        return jsonify({'error': 'Username already exists'}), 409
+
+    new_customer = Customer(
+        username=data['username'],
+        full_name=data['full_name'],
+        age=data['age'],
+        address=data['address'],
+        gender=data['gender'],
+        marital_status=data['marital_status']
+    )
+    new_customer.set_password(data['password'])
+
+    db.session.add(new_customer)
+    db.session.commit()
+    return jsonify({'message': 'Customer registered successfully'}), 201
+
+@app.route('/delete/<username>', methods=['DELETE'])
+def delete_customer(username):
+    customer = db.session.get(Customer, username)
+    if not customer:
+        return jsonify({'error': 'Customer not found'}), 404
+    db.session.delete(customer)
+    db.session.commit()
+    return jsonify({'message': 'Customer deleted successfully'}), 200
+
+@app.route('/update/<username>', methods=['PUT'])
+def update_customer(username):
+    customer = db.session.get(Customer, username)
+    if not customer:
+        return jsonify({'error': 'Customer not found'}), 404
+
+    data = request.json
+    if 'full_name' in data:
+        customer.full_name = data['full_name']
+    if 'age' in data:
+        customer.age = data['age']
+    if 'address' in data:
+        customer.address = data['address']
+    if 'gender' in data:
+        customer.gender = data['gender']
+    if 'marital_status' in data:
+        customer.marital_status = data['marital_status']
+    if 'password' in data:
+        customer.set_password(data['password'])
+
+    db.session.commit()
+    return jsonify({'message': 'Customer updated successfully'}), 200
+
+@app.route('/customers', methods=['GET'])
+def get_all_customers():
+    customers = Customer.query.all()
+    customer_list = [{'username': customer.username, 'full_name': customer.full_name, 'age': customer.age, 'address': customer.address, 'gender': customer.gender, 'marital_status': customer.marital_status, 'wallet': customer.wallet} for customer in customers]
+    return jsonify(customer_list), 200
+
+@app.route('/customer/<username>', methods=['GET'])
+def get_customer(username):
+    customer = db.session.get(Customer, username)
+    if not customer:
+        return jsonify({'error': 'Customer not found'}), 404
+    customer_info = {'username': customer.username, 'full_name': customer.full_name, 'age': customer.age, 'address': customer.address, 'gender': customer.gender, 'marital_status': customer.marital_status, 'wallet': customer.wallet}
+    return jsonify(customer_info), 200
+
+@app.route('/charge_wallet/<username>', methods=['POST'])
+def charge_wallet(username):
+    customer = db.session.get(Customer, username)
+    if not customer:
+        return jsonify({'error': 'Customer not found'}), 404
+
+    amount = request.json.get('amount', 0)
+    customer.wallet += amount
+    db.session.commit()
+    return jsonify({'message': f'{amount} added to wallet', 'new_balance': customer.wallet}), 200
+
+@app.route('/deduct_wallet/<username>', methods=['POST'])
+def deduct_wallet(username):
+    customer = db.session.get(Customer, username)
+    if not customer:
+        return jsonify({'error': 'Customer not found'}), 404
+
+    amount = request.json.get('amount', 0)
+    if customer.wallet < amount:
+        return jsonify({'error': 'Insufficient funds'}), 400
+    customer.wallet -= amount
+    db.session.commit()
+    return jsonify({'message': f'{amount} deducted from wallet', 'new_balance': customer.wallet}), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ customers/test_app.py
@@ -0,0 +1,141 @@
+import pytest
+from app import app, db, Customer  
+
+@pytest.fixture(scope='module')
+def test_client():
+    # Configure the app for testing
+    app.config['TESTING'] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    # Set up the test client
+    with app.test_client() as testing_client:
+        with app.app_context():
+            db.create_all()
+        yield testing_client  # This is where the testing happens
+
+        db.session.remove()
+        db.drop_all()
+@pytest.fixture(scope='module')
+def new_customer():
+    customer = Customer(username='testuser', full_name='Test User', password_hash='hashedpassword', age=30, address='123 Test St', gender='Male', marital_status='Single', wallet=100.0)
+    return customer
+
+# Model Tests
+def test_new_customer(new_customer):
+    assert new_customer.username == 'testuser'
+    assert new_customer.full_name == 'Test User'
+    assert new_customer.age == 30
+    assert new_customer.address == '123 Test St'
+    assert new_customer.gender == 'Male'
+    assert new_customer.marital_status == 'Single'
+    assert new_customer.wallet == 100.0
+
+# Endpoint Tests
+def test_home_page(test_client):
+    response = test_client.get('/')
+    assert response.status_code == 200
+    assert b"Welcome to the Customer Service API!" in response.data
+
+def test_register_customer(test_client):
+    # Success case
+    data = {'username': 'newuser', 'full_name': 'New User', 'password': 'newpassword', 'age': 25, 'address': '456 New St', 'gender': 'Female', 'marital_status': 'Single'}
+    response = test_client.post('/register', json=data)
+    assert response.status_code == 201
+    assert b'Customer registered successfully' in response.data
+
+    # Error case: Missing fields
+    data = {'username': 'incompleteuser'}
+    response = test_client.post('/register', json=data)
+    assert response.status_code == 400
+
+def test_update_customer(test_client, new_customer):
+    # Setup - Create a customer to update
+    db.session.add(new_customer)
+    db.session.commit()
+
+    # Success case
+    update_data = {'full_name': 'Updated Name'}
+    response = test_client.put(f'/update/{new_customer.username}', json=update_data)
+    assert response.status_code == 200
+    assert b'Customer updated successfully' in response.data
+
+    # Error case: Customer not found
+    response = test_client.put('/update/nonexistinguser', json=update_data)
+    assert response.status_code == 404
+
+def test_get_customers(test_client, new_customer):
+    # Setup - Create a customer
+    db.session.add(new_customer)
+    db.session.commit()
+    # Success case
+    response = test_client.get('/customers')
+    assert response.status_code == 200
+    customers_data = response.get_json()
+    assert len(customers_data) == 1
+    assert customers_data[0]['username'] == new_customer.username
+
+    # Error case: No customers found    
+    db.session.delete(new_customer)
+    db.session.commit()
+    response = test_client.get('/customers')
+    assert response.status_code == 404
+
+def test_get_customer(test_client, new_customer):
+    # Setup - Create a customer
+    db.session.add(new_customer)
+    db.session.commit()
+
+    # Success case
+    response = test_client.get(f'/customer/{new_customer.username}')
+    assert response.status_code == 200
+    customer_data = response.get_json()
+    assert customer_data['username'] == new_customer.username
+
+    # Error case: Customer not found
+    response = test_client.get('/customer/nonexistinguser')
+    assert response.status_code == 404
+
+def test_charge_wallet(test_client, new_customer):
+    # Setup - Create a customer
+    db.session.add(new_customer)
+    db.session.commit()
+
+    # Success case
+    charge_data = {'amount': 50}
+    response = test_client.post(f'/charge_wallet/{new_customer.username}', json=charge_data)
+    assert response.status_code == 200
+    assert b'added to wallet' in response.data
+
+    # Error case: Customer not found
+    response = test_client.post('/charge_wallet/nonexistinguser', json=charge_data)
+    assert response.status_code == 404
+
+def test_deduct_wallet(test_client, new_customer):
+    # Setup - Create a customer
+    db.session.add(new_customer)
+    db.session.commit()
+
+    # Success case
+    deduct_data = {'amount': 30}
+    response = test_client.post(f'/deduct_wallet/{new_customer.username}', json=deduct_data)
+    assert response.status_code == 200
+    assert b'deducted from wallet' in response.data
+
+    # Error case: Insufficient funds
+    insufficient_data = {'amount': 1000}
+    response = test_client.post(f'/deduct_wallet/{new_customer.username}', json=insufficient_data)
+    assert response.status_code == 400
+def test_delete_customer(test_client, new_customer):
+    # Setup - Create a customer to delete
+    db.session.add(new_customer)
+    db.session.commit()
+
+    # Success case
+    response = test_client.delete(f'/delete/{new_customer.username}')
+    assert response.status_code == 200
+    assert b'Customer deleted successfully' in response.data
+
+    # Error case: Customer not found
+    response = test_client.delete('/delete/nonexistinguser')
+    assert response.status_code == 404
