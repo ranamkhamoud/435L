dcd571fc8c0090fb055c81e23aea7639a2e5b11c
jww02 <jww02@mail.aub.edu>
2023-11-27 09:45:27 +0200

    added draft functions to sales service

--- sales/app.py
+++ sales/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from datetime import datetime #to save the history of purchases by a customer
+import requests #for accessing other APIs
 
 app = Flask(__name__)
 
@@ -29,12 +30,50 @@ class Sales(db.Model):
 with app.app_context():
     db.create_all()
 
+
+#to access customer api
+customer_service_balance =   # Replace with the actual URL
+
+
 #App Routes 
 
 @app.route('/')
 def home():
     return "Welcome to the Sales Service API!"
 
-#note: create app routes for displaying goods, getting the details for each good, and when a sale happens 
-    #needs to include if-statement
-    #include error statements
+
+@app.route('/display', methods=['GET'])
+def display_goods():
+    goods = Goods.query.all();
+        #loop over every item:
+    goods_list = [{'name': good.name, 'price': good.price} for good in goods_list]
+    return jsonify({'Goods' : goods_list})
+
+#get method (retrieve data)
+@app.route('/goods/<name>', methods=['GET'])
+def get_goods_info(name):
+    object = Goods.query.filter_by(name = object.name).first()
+
+    if object: #good is available
+        return jsonify({'name':object.name, 'price':object.price, 'count':object.totalAmount})
+    else:
+        return jsonify({'error': f'{name} not found in list of goods'}), 404
+
+#post method (submit data to be processed)
+@app.route('/sale', methods=['POST'])
+def sale_transaction():
+    data = request.json
+    good_name = data.get('name')
+    customer_user = data.get('customer_user')
+
+    object = Goods.query.filter_by(name='good_name').first()
+    
+    if object and object.totalAmount > 0 : #good is available
+        #check for money amount
+        #need to access customer wallet
+        return ##
+    else:
+        return ##
+
+if __name__ == "__main__":
+    app.run(debug=True)
