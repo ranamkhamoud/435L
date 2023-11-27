263f5d5d461f4601ce514aab30a75c014a07e74b
jww02 <jww02@mail.aub.edu>
2023-11-27 23:43:12 +0200

    completed draft of sales service

--- sales/app.py
+++ sales/app.py
@@ -23,7 +23,7 @@ class Goods(db.Model):
 class Sales(db.Model):
     username = db.Column(db.String(255), nullable = False)
     price = db.Column(db.Float, nullable = False)
-    goodName = db.Column(db.String(200), nullable = False)
+    name = db.Column(db.String(200), nullable = False)
     time = db.Column(db.DateTime, default=datetime.utcnow) #takes current time
 
 # Create declared tables
@@ -32,7 +32,7 @@ with app.app_context():
 
 
 #to access customer api
-customer_service_balance =   # Replace with the actual URL
+customer_service_balance = "http://localhost:5001/balance/<username>" # double check ports
 
 
 #App Routes 
@@ -46,7 +46,7 @@ def home():
 def display_goods():
     goods = Goods.query.all();
         #loop over every item:
-    goods_list = [{'name': good.name, 'price': good.price} for good in goods_list]
+    goods_list = [{'name': good.name, 'price': good.price} for good in goods]
     return jsonify({'Goods' : goods_list})
 
 #get method (retrieve data)
@@ -68,12 +68,47 @@ def sale_transaction():
 
     object = Goods.query.filter_by(name='good_name').first()
     
-    if object and object.totalAmount > 0 : #good is available
-        #check for money amount
-        #need to access customer wallet
-        return ##
+    if not object and object.totalAmount <= 0 : #good is not available
+        return jsonify({'error': 'Item not available'}), 404
+    
+    try:
+        customer_service_amount = {'customer_user': customer_user, 'required_amount': object.price}
+        balance_output = requests.post(customer_service_balance, json=customer_service_amount)
+        balance_data = balance_output.json()
+
+        if balance_output.status_code == 200:
+            object.totalAmount -= 1
+
+            sale = Sales(username=customer_user, name=good_name, price=object.price, time=datetime.utcnow())
+            db.session.add(sale)
+            db.session.commit()
+
+            return jsonify({'message': 'Sale successful', 'new_balance': balance_data.get('new_balance')}), 200
+        else:
+            requests.post(f"{customer_service_balance}/charge_wallet/{customer_user}", json={'amount': object.price})
+            return jsonify({'error': 'Customer has insufficient funds'}), 400
+
+    except requests.exceptions.RequestException as e:
+        return jsonify({'error': f'Error connecting to customer service: {e}'}), 500
+
+# additional route in case of sales history 
+@app.route('/sales-history/<username>', methods=['GET'])
+def get_sales_history(username):
+    history = Sales.query.filter_by(username=username).all()
+
+    if history:
+        formatted_sales_history = [
+            {
+                'good': sale.name,
+                'price': sale.price,
+                'timestamp': sale.timestamp.strftime('%Y-%m-%d %H:%M:%S')
+            }
+            for sale in history
+        ]
+        return jsonify({'sales_history': formatted_sales_history})
     else:
-        return ##
+        return jsonify({'message': f'No sales history found for {username}'})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
