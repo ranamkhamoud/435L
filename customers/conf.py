7370eb1dc88b2980fe2b2b33e5b0d6d59b11e518
jww02 <jww02@mail.aub.edu>
2023-11-28 21:17:31 +0200

    customer documentation added

---
+++ customers/conf.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+# Add the path to the API script directory
+sys.path.insert(0, os.path.abspath('../'))
+
+# ... (existing conf.py content)
+
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CustomerService'
+copyright = '2023, Jana'
+author = 'Jana'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+]
